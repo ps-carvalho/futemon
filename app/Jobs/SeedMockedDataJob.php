5ad6e62dbd615bc1cdf65e418172dacf246afa75
Paulo Carvalho <paulofrediani@icloud.com>
2025-06-29 17:45:11 +0100

    feat: add cached value after import is completed

--- app/Jobs/SeedMockedDataJob.php
+++ app/Jobs/SeedMockedDataJob.php
@@ -1,7 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Jobs;
 
+use Exception;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -9,10 +12,14 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Cache;
+use Log;
 
-class SeedMockedDataJob implements ShouldQueue
+final class SeedMockedDataJob implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
 
     public function __construct()
     {
@@ -27,11 +34,12 @@ class SeedMockedDataJob implements ShouldQueue
 
             // Mark job as completed
             Cache::put('setup_job_completed_mocked_data', true, now()->addMinutes(10));
+            Cache::put('data_populated', true, now()->addMinutes(2400));
 
-        } catch (\Exception $e) {
+        } catch (Exception $exception) {
             // Handle error
-            \Log::error('Mocked data seeding failed: ' . $e->getMessage());
-            throw $e;
+            Log::error('Mocked data seeding failed: '.$exception->getMessage());
+            throw $exception;
         }
     }
 }
