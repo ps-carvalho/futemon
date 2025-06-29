42337757ecca0bad1d61e2e2e68dbd7ca6d16e2b
Paulo Carvalho <paulofrediani@icloud.com>
2025-06-29 15:20:01 +0100

    refactor: removed redundant route

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PlayersController::class, 'index'])->name('welcome');
 Route::get('/setup', [PlayersController::class, 'setup'])->name('setup');
 Route::get('/players', [PlayersController::class, 'players'])->name('players');
-Route::get('/players/{id}', [PlayersController::class, 'show'])->name('players.show');
